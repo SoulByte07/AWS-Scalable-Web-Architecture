da5529e02229a83d48102f5307ffc012493ef990
soulbyte07 <soul3147@proton.me>
2026-03-23 11:53:01 +0530

    route_table.tf : /home/soul igw var name

--- Terraform/Network/route_table.tf
+++ Terraform/Network/route_table.tf
@@ -3,7 +3,7 @@ resource "aws_route_table" "main_rt" {
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.main.id 
+    gateway_id = aws_internet_gateway.igw.id 
   }
   tags = {
     Name = "main_route_table"
