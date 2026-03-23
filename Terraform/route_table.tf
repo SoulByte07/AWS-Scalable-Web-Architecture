e752fc9d2e56a43f1e738a5f297d66896c789be1
soulbyte07 <soul3147@proton.me>
2026-03-23 11:37:36 +0530

    route_table.tf : init

---
+++ Terraform/route_table.tf
@@ -0,0 +1,11 @@
+resource "aws_route_table" "main_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id 
+  }
+  tags = {
+    Name = "main_route_table"
+  }
+}
