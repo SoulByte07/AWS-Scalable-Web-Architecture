3892b16f5d1fa550744bf588e9111efec7db022a
soulbyte07 <soul3147@proton.me>
2026-03-24 19:24:51 +0530

    rdb.tf : inir

---
+++ Terraform/Storage/rdb.tf
@@ -0,0 +1,10 @@
+terraform "aws_rdb_instance" "rdb" {
+  allocated_storage    = 20
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t2.micro"
+  name                 = "mydb"
+  username             = "admin"
+  password             = "password"
+  parameter_group_name = "default.mysql5.7"
+}
