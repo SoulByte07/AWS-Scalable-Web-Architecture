c864ea7bfdd5dcb3403cfc8dbb9ef85b961107d4
soulbyte07 <soul3147@proton.me>
2026-03-24 19:18:08 +0530

    security-group.tf: init

---
+++ Terraform/Security/security-group.tf
@@ -0,0 +1,11 @@
+terraform "aws_security_group" "web_sg" {
+  name        = "web-sg"
+  description = "webserver security group"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+}
