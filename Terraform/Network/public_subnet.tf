3abda697304b4bd0d7bef818638548eb6f2f8134
soulbyte07 <soul3147@proton.me>
2026-04-02 10:22:20 +0530

    Network/public_subnet.tf: + var.AZ

--- Terraform/Network/public_subnet.tf
+++ Terraform/Network/public_subnet.tf
@@ -1,12 +1,15 @@
+# Network/public_subnet.tf
 resource "aws_subnet" "public_subnet" {
+  count             = length(var.availability_zones) # Loops twice
   vpc_id            = aws_vpc.main_vpc.id
-  cidr_block        = "10.0.1.0/24"
+  
+  # Generates 10.0.1.0/24 and 10.0.2.0/24
+  cidr_block        = "10.0.${count.index + 1}.0/24" 
+  availability_zone = var.availability_zones[count.index]
 
   map_public_ip_on_launch = true 
 
-  availability_zone = ["ap-south-1a", "ap-south-1b"][count.index]
-
   tags = {
-    Name = "Public Subnet"
+    Name = "Public Subnet ${var.availability_zones[count.index]}"
   }
 }
