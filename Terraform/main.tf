2040cfdabd920c658b803e9758f0f94c670b7014
soulbyte07 <soul3147@proton.me>
2026-04-02 16:16:33 +0530

    main.tf : + private_subnet_ids, db_security_group_id

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,31 +1,28 @@
 # Description: The master file that calls all sub-modules and passes data between them.
 
 module "networking" {
-  # Tells Terraform where to find the code
   source = "./Network" 
 }
 
 module "security" {
   source = "./Security"
-  # Passes the VPC ID from the networking module into the security module
   vpc_id = module.networking.vpc_id 
 }
 
 module "compute" {
   source = "./Compute"
-  # Passes data from network and security into compute
-  vpc_id            = module.networking.vpc_id
-  security_group_id = module.security.app_sg_id 
+  vpc_id                = module.networking.vpc_id
+  private_subnet_ids    = module.networking.private_subnet_ids 
+  alb_security_group_id = module.security.alb_strict_sg_id     
 }
 
 module "database" {
   source = "./Storage"
-  # Passes the VPC ID from the networking module into the database module
-  vpc_id = module.networking.vpc_id 
+  private_db_subnets   = module.networking.private_subnet_ids
+  db_security_group_id = module.security.db_sg_id
 }
 
 module "edge" {
   source = "./Edge"
-  # Passes the VPC ID from the networking module into the edge module
-  vpc_id = module.networking.vpc_id 
 }
+
