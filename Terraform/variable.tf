0988e525f05d34b87ba9107e3669228ba183255c
soulbyte07 <soul3147@proton.me>
2026-04-02 10:20:15 +0530

    Terraform/provider.tf : + AZ

--- Terraform/variable.tf
+++ Terraform/variable.tf
@@ -1,11 +1,16 @@
-variable "instace_type" {
-  type = string
+# variable.tf
+variable "availability_zones" {
+  description = "AZs for High Availability"
+  type        = list(string)
+  default     = ["ap-south-1a", "ap-south-1b"]
+}
+
+variable "instance_type" { 
+  type    = string
   default = "t2.micro"
 } 
 
 variable "ami_id" {
-  type = string
+  type    = string
   default = "ami-0c94855ba95c71c99"
 }
-
-
