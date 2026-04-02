563ae66e07be8ec706075131e11a8f1a4bc1713f
soulbyte07 <soul3147@proton.me>
2026-04-02 16:21:53 +0530

    Network/elastic-ip.tf: /home/soul elastic_ip to eip

--- Terraform/Network/elastic-ip.tf
+++ Terraform/Network/elastic-ip.tf
@@ -1,4 +1,4 @@
-resource "elastic_ip" "this" {
+resource "eip" "this" {
   count = var.create_elastic_ip ? 1 : 0
 
   vpc = true
--- Terraform/Storage/remote-terraform-state.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "terraform-state-bucket"
-    key            = "terraform/terraform.tfstate"
-    region         = "ap-south-1"
-    dynamodb_table = "state-locks" 
-    encrypt        = true
-  }
-}
