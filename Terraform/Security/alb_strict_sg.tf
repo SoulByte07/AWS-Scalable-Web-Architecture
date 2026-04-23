d2e0747ac87470bcfb95e930791375ab837afa77
soulbyte07 <soul3147@proton.me>
2026-04-23 12:12:14 +0530

    security/alb strict: + https, - hardcode prefix

--- Terraform/Security/alb_strict_sg.tf
+++ Terraform/Security/alb_strict_sg.tf
@@ -1,21 +1,30 @@
+data "aws_ec2_managed_prefix_list" "cloudfront_origin_facing" {
+  name = "com.amazonaws.global.cloudfront.origin-facing"
+}
+
 resource "aws_security_group" "alb_strict_sg" {
   name        = "vocal4local-alb-strict-sg"
   description = "Allow HTTPS from CloudFront ONLY"
   vpc_id      = var.vpc_id
 
   ingress {
-    from_port = 443
-    to_port   = 443
-    protocol  = "tcp"
-    # AWS Managed Prefix List for CloudFront (Global)
-    prefix_list_ids = ["pl-3b927c52"]
+    from_port       = 443
+    to_port         = 443
+    protocol        = "tcp"
+    prefix_list_ids = [data.aws_ec2_managed_prefix_list.cloudfront_origin_facing.id]
+  }
+
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    prefix_list_ids = [data.aws_ec2_managed_prefix_list.cloudfront_origin_facing.id]
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
   }
 }
-
