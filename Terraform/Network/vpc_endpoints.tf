2f2fb13f89d3e4a55f95e217e66ad75ef147d128
soulbyte07 <soul3147@proton.me>
2026-04-27 20:14:52 +0530

    net/vpc_endpoint: init

---
+++ Terraform/Network/vpc_endpoints.tf
@@ -0,0 +1,85 @@
+resource "aws_security_group" "vpce_sg" {
+  name        = "vocal4local-vpce-sg"
+  description = "Allow HTTPS from app subnets to VPC interface endpoints"
+  vpc_id      = aws_vpc.main_vpc.id
+
+  ingress {
+    description = "HTTPS from private app subnets"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = [for s in aws_subnet.private_subnet : s.cidr_block]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_vpc_endpoint" "s3_gateway" {
+  vpc_id            = aws_vpc.main_vpc.id
+  service_name      = "com.amazonaws.${data.aws_region.current.name}.s3"
+  vpc_endpoint_type = "Gateway"
+  route_table_ids   = [for rt in aws_route_table.private_rt : rt.id]
+
+  tags = {
+    Name = "vocal4local-s3-gateway-endpoint"
+  }
+}
+
+resource "aws_vpc_endpoint" "ssm" {
+  vpc_id              = aws_vpc.main_vpc.id
+  service_name        = "com.amazonaws.${data.aws_region.current.name}.ssm"
+  vpc_endpoint_type   = "Interface"
+  subnet_ids          = [for s in aws_subnet.private_subnet : s.id]
+  security_group_ids  = [aws_security_group.vpce_sg.id]
+  private_dns_enabled = true
+
+  tags = {
+    Name = "vocal4local-ssm-endpoint"
+  }
+}
+
+resource "aws_vpc_endpoint" "ssmmessages" {
+  vpc_id              = aws_vpc.main_vpc.id
+  service_name        = "com.amazonaws.${data.aws_region.current.name}.ssmmessages"
+  vpc_endpoint_type   = "Interface"
+  subnet_ids          = [for s in aws_subnet.private_subnet : s.id]
+  security_group_ids  = [aws_security_group.vpce_sg.id]
+  private_dns_enabled = true
+
+  tags = {
+    Name = "vocal4local-ssmmessages-endpoint"
+  }
+}
+
+resource "aws_vpc_endpoint" "ec2messages" {
+  vpc_id              = aws_vpc.main_vpc.id
+  service_name        = "com.amazonaws.${data.aws_region.current.name}.ec2messages"
+  vpc_endpoint_type   = "Interface"
+  subnet_ids          = [for s in aws_subnet.private_subnet : s.id]
+  security_group_ids  = [aws_security_group.vpce_sg.id]
+  private_dns_enabled = true
+
+  tags = {
+    Name = "vocal4local-ec2messages-endpoint"
+  }
+}
+
+resource "aws_vpc_endpoint" "logs" {
+  vpc_id              = aws_vpc.main_vpc.id
+  service_name        = "com.amazonaws.${data.aws_region.current.name}.logs"
+  vpc_endpoint_type   = "Interface"
+  subnet_ids          = [for s in aws_subnet.private_subnet : s.id]
+  security_group_ids  = [aws_security_group.vpce_sg.id]
+  private_dns_enabled = true
+
+  tags = {
+    Name = "vocal4local-logs-endpoint"
+  }
+}
+
+data "aws_region" "current" {}
