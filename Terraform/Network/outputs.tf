37c8d6fcb891e742c884527e53a302d878478b34
soulbyte07 <soul3147@proton.me>
2026-04-27 20:14:22 +0530

    net/out: + vpc cidr

--- Terraform/Network/outputs.tf
+++ Terraform/Network/outputs.tf
@@ -5,6 +5,10 @@ output "vpc_id" {
   value = aws_vpc.main_vpc.id
 }
 
+output "vpc_cidr" {
+  value = aws_vpc.main_vpc.cidr_block
+}
+
 output "public_subnet_ids" {
   value = [for s in aws_subnet.public_subnet : s.id]
 }
