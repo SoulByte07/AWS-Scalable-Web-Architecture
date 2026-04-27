76a4d6bc041cdf0b28824ca03dea9382b710a15f
soulbyte07 <soul3147@proton.me>
2026-04-27 19:11:09 +0530

    security: + allow inbound web traffic to alb

--- Terraform/Security/alb_strict_sg.tf
+++ Terraform/Security/alb_strict_sg.tf
@@ -1,24 +1,20 @@
-data "aws_ec2_managed_prefix_list" "cloudfront_origin_facing" {
-  name = "com.amazonaws.global.cloudfront.origin-facing"
-}
-
 resource "aws_security_group" "alb_strict_sg" {
   name        = "vocal4local-alb-strict-sg"
-  description = "Allow HTTPS from CloudFront ONLY"
+  description = "Allow inbound web traffic to ALB"
   vpc_id      = var.vpc_id
 
   ingress {
-    from_port       = 443
-    to_port         = 443
-    protocol        = "tcp"
-    prefix_list_ids = [data.aws_ec2_managed_prefix_list.cloudfront_origin_facing.id]
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port       = 80
-    to_port         = 80
-    protocol        = "tcp"
-    prefix_list_ids = [data.aws_ec2_managed_prefix_list.cloudfront_origin_facing.id]
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
