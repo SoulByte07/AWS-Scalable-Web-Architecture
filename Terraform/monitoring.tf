e1897b21c725a9a320119facea87ce65294b4391
soulbyte07 <soul3147@proton.me>
2026-04-27 19:15:00 +0530

    monitoring: + health check

---
+++ Terraform/monitoring.tf
@@ -0,0 +1,75 @@
+resource "aws_sns_topic" "ops_alerts" {
+  name = "vocal4local-ops-alerts"
+}
+
+resource "aws_cloudwatch_metric_alarm" "alb_5xx" {
+  alarm_name          = "vocal4local-alb-5xx"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 1
+  metric_name         = "HTTPCode_ELB_5XX_Count"
+  namespace           = "AWS/ApplicationELB"
+  period              = 60
+  statistic           = "Sum"
+  threshold           = 5
+  treat_missing_data  = "notBreaching"
+  alarm_description   = "ALB has elevated 5XX responses"
+  alarm_actions       = [aws_sns_topic.ops_alerts.arn]
+
+  dimensions = {
+    LoadBalancer = module.compute.alb_arn_suffix
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "asg_cpu_high" {
+  alarm_name          = "vocal4local-asg-cpu-high"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 2
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = 120
+  statistic           = "Average"
+  threshold           = 80
+  treat_missing_data  = "notBreaching"
+  alarm_description   = "Average ASG instance CPU is high"
+  alarm_actions       = [aws_sns_topic.ops_alerts.arn]
+
+  dimensions = {
+    AutoScalingGroupName = module.compute.asg_name
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "rds_cpu_high" {
+  alarm_name          = "vocal4local-rds-cpu-high"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 2
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/RDS"
+  period              = 120
+  statistic           = "Average"
+  threshold           = 80
+  treat_missing_data  = "notBreaching"
+  alarm_description   = "RDS CPU utilization is high"
+  alarm_actions       = [aws_sns_topic.ops_alerts.arn]
+
+  dimensions = {
+    DBInstanceIdentifier = module.database.db_instance_identifier
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "rds_free_storage_low" {
+  alarm_name          = "vocal4local-rds-free-storage-low"
+  comparison_operator = "LessThanThreshold"
+  evaluation_periods  = 1
+  metric_name         = "FreeStorageSpace"
+  namespace           = "AWS/RDS"
+  period              = 300
+  statistic           = "Average"
+  threshold           = 2147483648
+  treat_missing_data  = "breaching"
+  alarm_description   = "RDS free storage below 2 GiB"
+  alarm_actions       = [aws_sns_topic.ops_alerts.arn]
+
+  dimensions = {
+    DBInstanceIdentifier = module.database.db_instance_identifier
+  }
+}
