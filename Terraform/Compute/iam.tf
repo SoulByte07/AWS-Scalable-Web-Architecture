bb2dffcf89dfe1bf6a20cf450d6f163ec5409887
soulbyte07 <soul3147@proton.me>
2026-04-27 19:05:02 +0530

    iam: init

---
+++ Terraform/Compute/iam.tf
@@ -0,0 +1,32 @@
+data "aws_iam_policy_document" "ec2_assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "ec2_ssm_cloudwatch" {
+  name               = "vocal4local-ec2-ssm-cloudwatch-role"
+  assume_role_policy = data.aws_iam_policy_document.ec2_assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "ssm_core" {
+  role       = aws_iam_role.ec2_ssm_cloudwatch.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_role_policy_attachment" "cloudwatch_agent" {
+  role       = aws_iam_role.ec2_ssm_cloudwatch.name
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
+}
+
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = "vocal4local-ec2-profile"
+  role = aws_iam_role.ec2_ssm_cloudwatch.name
+}
